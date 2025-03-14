2f0e4f35b8a757043dbadbf74db51a2ba6224861
= <lucasignaciodencor@gmail.com>
2025-03-13 22:29:44 -0300

    renombrar clases al formato react

--- src/pages/public/Encyclopedia/Encyclopedia.tsx
+++ src/pages/public/Encyclopedia/Encyclopedia.tsx
@@ -1,31 +1,35 @@
 import "./Encyclopedia.css";
 import { Nav } from "../../../components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Encyclopedia = () => {
   return (
     <div>
       <Nav />
-      <div class="encyclopedia-container">
-        <div class="arrow-previous"><Link to="/map">.</Link></div>
-        <div class="title">Triassic Period</div>
-        <div class="dino-card-container">
-          <div class="dino-card">
-            <div class="dinosaur dinosaur-1"></div>
-          </div>
-          <div class="dino-card dinosaur-2"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
-          <div class="dino-card"></div>
+      <div className="encyclopedia-container">
+        <div className="arrow-previous">
+          <Link to="/map">.</Link>
         </div>
-        <div class="arrow-next"><Link to="/triassic-medio">.</Link></div>
+        <div className="title">Triassic Period</div>
+        <div className="dino-card-container">
+          <div className="dino-card">
+            <div className="dinosaur dinosaur-1"></div>
+          </div>
+          <div className="dino-card dinosaur-2"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+          <div className="dino-card"></div>
+        </div>
+        <div className="arrow-next">
+          <Link to="/triassic-medio">.</Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/public/Home/Home.tsx
+++ src/pages/public/Home/Home.tsx
@@ -8,75 +8,75 @@ export const Home = () => {
       <Nav />
       <header>HEADER</header>
       <div className="periods-container">
-        <div class="period triassic">
-          <div className="title">Triassic<br />Period</div>
-          <div class="description">
-            <i
-            >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum."<br /><br />"Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat."</i
-            >
+        <div className="period triassic">
+          <div className="title">
+            Triassic
+            <br />
+            Period
           </div>
-          <div class="preiod-title"></div>
-        </div>
-        <div class="period jurassic">
-          <div class="title">Jurassic<br />Period</div>
-          <div class="description">
-            <i
-            >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum."<br /><br />"Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat."</i
-            >
+          <div className="description">
+            <i>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              <br />
+              <br />
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat."
+            </i>
           </div>
         </div>
-        <div class="period cretaceous">
-          <div class="title">Cretaceous<br />Period</div>
-          <div class="description">
-            <i
-            >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum."<br /><br />"Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat."</i
-            >
+        <div className="period jurassic">
+          <div className="title">
+            Jurassic
+            <br />
+            Period
+          </div>
+          <div className="description">
+            <i>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat."
+            </i>
+          </div>
+        </div>
+        <div className="period cretaceous">
+          <div className="title">
+            Cretaceous
+            <br />
+            Period
+          </div>
+          <div className="description">
+            <i>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat."
+            </i>
           </div>
         </div>
       </div>
       <div className="map-btn">
         <Link to="/map">Go to Map</Link>
       </div>
-      {/* <div class="plant-1"></div>
-      <div class="plant-2"></div> */}
 
-      <div class="paw-1"></div>
-      <div class="paw-2"></div>
-      <div class="paw-3"></div>
-      <div class="paw-4"></div>
-      <div class="paw-5"></div>
-      <div class="paw-6"></div>
-      <div class="paw-7"></div>
+      <div className="plant-1"></div>
+      <div className="plant-2"></div>
+
+      <div className="paw-1"></div>
+      <div className="paw-2"></div>
+      <div className="paw-3"></div>
+      <div className="paw-4"></div>
+      <div className="paw-5"></div>
+      <div className="paw-6"></div>
+      <div className="paw-7"></div>
     </div>
   );
 };
