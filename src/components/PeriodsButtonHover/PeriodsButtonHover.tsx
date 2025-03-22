bd6382850998a4e11fbb865b8234fa5cc0fe3f74
Hunny <unreal5_gonza@outlook.es>
2025-03-21 22:48:47 -0300

    Agrega soporte del componente para no tener dinos.

--- src/components/PeriodsButtonHover/PeriodsButtonHover.tsx
+++ src/components/PeriodsButtonHover/PeriodsButtonHover.tsx
@@ -5,7 +5,7 @@ interface PeriodsButtonHoverProps {
   stage: string;
   label: string;
   link: string;
-  dinos: string[];
+  dinos?: string[];
   infoOrientation?: string;
 }
 
@@ -18,40 +18,42 @@ export const PeriodsButtonHover: React.FC<PeriodsButtonHoverProps> = ({
 }) => {
   const [hovered, setHovered] = useState(false);
 
-  return (
-      <div
-          className={`period-btn ${stage} ${hovered ? "scaled" : ""}`}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-      >
-        
-        <div className={`period-btn-bg ${stage}-bg`}></div>
-        {hovered && (
-            [<div
-                className={`info-container fade-in ${infoOrientation}`}
-                onMouseEnter={() => setHovered(true)}
-            >
-              <div className="info-frame">
-                <div className="site-description">
-                  <span>{label}</span>
-                </div>
+    return (
+        <div
+            className={`period-btn ${stage} ${hovered ? "scaled" : ""}`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
 
-                <button>
-                  <Link to={link}>VISIT</Link>
-                </button>
-              </div>
-            </div>,
-            <div className={`paper ${infoOrientation}`}>
-              <div className="info-triassic-dinos-container">
-                {dinos.map((dino: string, index: number) => (
-                    <div
-                        key={index}
-                        className={`${dino} dinosaur-silhouette`}
-                    ></div>
-                ))}
-              </div>
-            </div>]
-        )}
-      </div>
-  );
+            <div className={`period-btn-bg ${stage}-bg`}></div>
+            {hovered && (
+                [<div
+                    className={`info-container fade-in ${infoOrientation}`}
+                    onMouseEnter={() => setHovered(true)}
+                >
+                    <div className="info-frame">
+                        <div className="site-description">
+                            <span>{label}</span>
+                        </div>
+
+                        <button>
+                            <Link to={link}>VISIT</Link>
+                        </button>
+                    </div>
+                </div>,
+                    dinos && dinos.length > 0 ?
+                        <div className={`paper ${infoOrientation}`}>
+                            <div className="info-triassic-dinos-container">
+                                {dinos.map((dino: string, index: number) => (
+                                    <div
+                                        key={index}
+                                        className={`${dino} dinosaur-silhouette`}
+                                    ></div>
+                                ))}
+                            </div>
+                        </div> : null
+                ]
+            )}
+        </div>
+    );
 };
