e958721cf7e3261ec726e55ad5bfa334f976e521
Hunny <gonzalo.contogrobly@gmail.com>
2025-04-15 20:10:05 -0300

    Cambia el nav para adaptarlo a la nueva estetica.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
-export const Nav: React.FC = () => {
+export const Nav= () => {
   return (
     <nav>
-      <div className="profile-container"></div>
+      <div className="profile-container">
+          <div className="profile"></div>
+          <div className="pawn-1 completed"></div>
+          <div className="pawn-2 completed"></div>
+          <div className="pawn-3 completed"></div>
+          <div className="pawn-4"></div>
+          <div className="pawn-5"></div>
+          <div className="pawn-6"></div>
+          <div className="pawn-7"></div>
+          <div className="pawn-8"></div>
+      </div>
       <div className="routes-bg">
         <div className="routes">
-          <Link to="/">Home</Link>
-          <Link to="/map">Map</Link>
-          <Link to="/encyclopedia">Encyclopedia</Link>
-          <Link to="/album">Album</Link>
-          {/* <Link to="/login">Login</Link> */}
+          <Link to="/">HOME</Link>
+          <Link to="/map">MAP</Link>
+          <Link to="/store">STORE</Link>
+          <Link to="/profile">PROFILE</Link>
+          <Link to="/album">ALBUM</Link>
+          <Link to="/games">GAMES</Link>
+          <Link to="/library">LIBRARY</Link>
+          <Link to="/cinema">CINEMA</Link>
         </div>
       </div>
     </nav>
