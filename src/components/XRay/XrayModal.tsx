ffc3100b7ee70db560deb2b46633a6e783d4fffc
Gemdelle <milu200911@hotmail.com>
2025-04-29 21:00:27 -0300

    x-ray puzzle piece.

---
+++ src/components/XRay/XrayModal.tsx
@@ -0,0 +1,152 @@
+import React, {useEffect, useState} from "react";
+
+// IMPORTA tus estilos
+import stylesContainer from "../../pages/public/Triassic-Inferior/Triassic-Inferior.module.css"; // <- Estilos de la página
+import styles from "./XRayModal.module.css"; // <- Estilos del componente
+
+type XRayModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  selectedDinosaur: number | null;
+  activeDinosaur: number | null;
+  setActiveDinosaur: (index: number | null) => void;
+};
+
+export const XRayModal: React.FC<XRayModalProps> = ({
+  isOpen,
+  onClose,
+  selectedDinosaur,
+  activeDinosaur,
+  setActiveDinosaur
+}) => {
+  const [showPuzzlePiece, setShowPuzzlePiece] = useState(true);
+  const [pieceLeftPercent, setPieceLeftPercent] = useState(0.8);
+  const [pieceTopPercent, setPieceTopPercent] = useState(0.5);
+
+  if (!isOpen || selectedDinosaur === null) return null;
+
+  const setPuzzlePiecePosition = (activeDinosaur: number | null) => {
+    if(activeDinosaur === 0) {
+      setPieceLeftPercent(0.8);
+      setPieceTopPercent(0.5);
+    }
+    if(activeDinosaur === 1) {
+      setPieceLeftPercent(0.8);
+      setPieceTopPercent(0.3);
+    }
+    if(activeDinosaur === 2) {
+      setPieceLeftPercent(0.4);
+      setPieceTopPercent(0.5);
+    }
+  }
+
+  useEffect(() => {
+    setPuzzlePiecePosition(activeDinosaur);
+  }, [activeDinosaur]); 
+
+  const handleMouseMove = (
+    mouseEvent: React.MouseEvent<HTMLDivElement>,
+    index: number
+  ) => {
+    const rect = mouseEvent.currentTarget.getBoundingClientRect();
+
+    const x_percent = ((mouseEvent.clientX - rect.left) / rect.width) * 100;
+    const y_percent = ((mouseEvent.clientY - rect.top) / rect.height) * 100;
+    mouseEvent.currentTarget.style.setProperty("--cursor-x", `${x_percent}%`);
+    mouseEvent.currentTarget.style.setProperty("--cursor-y", `${y_percent}%`);
+
+    // Lógica de “proximidad”
+    // Coordenadas del mouse dentro del contenedor
+    const mouseX = mouseEvent.clientX - rect.left;
+    const mouseY = mouseEvent.clientY - rect.top;
+
+    // Por ejemplo: centro del dinosaurio,
+    // o cámbialo por cualquier coordenada objetivo de tu "hueso"
+    const targetX = rect.width / 2;
+    const targetY = rect.height / 2;
+
+    const pieceX = rect.width * pieceLeftPercent;
+    const pieceY = rect.height * pieceTopPercent;
+
+    const distance = Math.sqrt(
+      Math.pow(mouseX - pieceX, 2) + Math.pow(mouseY - pieceY, 2)
+    );
+
+    // si la distancia del mouse es menor a 20px, se activa el estado para mostrar la piece
+    if (distance < 22) {
+      setShowPuzzlePiece(true);
+    } else {
+      setShowPuzzlePiece(false);
+    }
+
+    for (let i = 0; i < mouseEvent.currentTarget.getElementsByClassName(stylesContainer.dinosaur).length; i++) {
+      const dinosaur = mouseEvent.currentTarget.getElementsByClassName(stylesContainer.dinosaur)[i] as HTMLElement;
+      const dRect = dinosaur.getBoundingClientRect();
+      if (
+        mouseEvent.clientX >= dRect.left &&
+        mouseEvent.clientX <= dRect.right &&
+        mouseEvent.clientY >= dRect.top &&
+        mouseEvent.clientY <= dRect.bottom
+      ) {
+        setActiveDinosaur(index);
+        return;
+      }
+    }
+    setActiveDinosaur(null);
+  };
+
+  const dinosaurBoneBgClasses = [
+    stylesContainer.dinosaurBoneBg1,
+    stylesContainer.dinosaurBoneBg2,
+    stylesContainer.dinosaurBoneBg3,
+  ];
+
+  return (
+    <div className={stylesContainer.modalOverlay + " preview-scan-dino"} onClick={onClose}>
+      <div
+        className={stylesContainer.modalContent}
+        onClick={e => e.stopPropagation()}
+      >
+        <div className={stylesContainer.dinosaurContainer}>
+          <h2>{`Dinosaurio ${selectedDinosaur + 1}`}</h2>
+          <div
+            key={`preview-${selectedDinosaur}`}
+            className={dinosaurBoneBgClasses[selectedDinosaur]}
+            onMouseMove={e => handleMouseMove(e, selectedDinosaur)}
+          >
+            <div
+              className={
+                `${stylesContainer.dinosaur} ${stylesContainer[`dinosaur${selectedDinosaur + 1}`]} ` +
+                `${activeDinosaur === selectedDinosaur ? stylesContainer.activeBone : ""}`
+              }
+            >
+              <div className={styles.ContainerPuzzlePiece}>
+              <div
+                className={
+                  `${stylesContainer[`dinosaur${selectedDinosaur + 1}Bone`]} ` +
+                  `${activeDinosaur === selectedDinosaur ? stylesContainer.activeBone : ""}`
+                }
+              ></div>
+              {showPuzzlePiece && (
+                <img
+                  className={styles.PuzzlePiece}
+                  src="../../../public/assets/img/puzzles/puzzle-piece.png"
+                  style={{
+                    pointerEvents: 'none',
+                    position: 'absolute',
+                    top: `${pieceTopPercent * 100}%`,
+                    left: `${pieceLeftPercent * 100}%`,
+                    transform: 'translate(-50%, -50%)',                
+                    zIndex: 10
+                  }}
+                  alt="puzzle piece"
+                />
+              )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/public/Triassic-Inferior/Triassic-Inferior.tsx
+++ src/pages/public/Triassic-Inferior/Triassic-Inferior.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { GalleryArrows } from "../../../components/GalleryArrows/GalleryArrows";
 import { GalleryDinosaurNames } from "../../../components/GalleryDinosaurNames/GalleryDinosaurNames";
 import galleries_data from "../../../context/data/galleries_data.json";
+import { XRayModal } from "../../../components/XRay/XrayModal";
 
 // const dinosaurs = galleries_data["galleries"][0][]
 
@@ -18,38 +19,6 @@ export const TriassicInferior = () => {
   const [second1Passed, setSecond1Passed] = useState<boolean>(false);
   const [second2Passed, setSecond2Passed] = useState<boolean>(false);
   const [second3Passed, setSecond3Passed] = useState<boolean>(false);
-
-  const handleMouseMove = (
-    mouseEvent: React.MouseEvent<HTMLDivElement>,
-    index: number
-  ) => {
-    const rect = mouseEvent.currentTarget.getBoundingClientRect();
-
-    const x = `${((mouseEvent.clientX - rect.left) / rect.width) * 100}%`;
-    const y = `${((mouseEvent.clientY - rect.top) / rect.height) * 100}%`;
-
-    mouseEvent.currentTarget.style.setProperty("--cursor-x", x);
-    mouseEvent.currentTarget.style.setProperty("--cursor-y", y);
-
-    const dinosaurElements = mouseEvent.currentTarget.getElementsByClassName(styles.dinosaur);
-
-    console.log(dinosaurElements);
-
-    for (let i = 0; i < dinosaurElements.length; i++) {
-      const dinosaur = dinosaurElements[i] as HTMLElement;
-      const rect = dinosaur.getBoundingClientRect();
-      if (
-        mouseEvent.clientX >= rect.left &&
-        mouseEvent.clientX <= rect.right &&
-        mouseEvent.clientY >= rect.top &&
-        mouseEvent.clientY <= rect.bottom
-      ) {
-        setActiveDinosaur(index);
-        return;
-      }
-    }
-    setActiveDinosaur(null);
-  };
 
   const handleDinosaurClick = (index: number) => {
     setSelectedDinosaur(index);
@@ -112,7 +81,7 @@ export const TriassicInferior = () => {
             <div
               key={index}
               className={bgClass}
-              onMouseMove={(e) => handleMouseMove(e, index)}
+              // onMouseMove={(e) => handleMouseMove(e, index)}
               onClick={() => handleDinosaurClick(index)}
             >
               <div className={`${styles.dinosaur} ${styles[`dinosaur${index + 1}`]}`}></div>
@@ -120,32 +89,16 @@ export const TriassicInferior = () => {
           );
         })}
 
-
-        {isModalOpen && (
-          <div className={styles.modalOverlay + " preview-scan-dino"} onClick={closeModal}>
-            <div
-              className={styles.modalContent}
-              onClick={(e) => e.stopPropagation()}
-            >
-              <h2>{`Dinosaurio ${selectedDinosaur !== null ? selectedDinosaur + 1 : ""}`}</h2>
-              <div
-                key={`preview-${selectedDinosaur}`}
-                className={[styles.dinosaurBg1, styles.dinosaurBg2, styles.dinosaurBg3][selectedDinosaur]}
-                onMouseMove={(e) => handleMouseMove(e, selectedDinosaur!)}
-              >
-                <div
-                  className={`${styles.dinosaur} ${styles[`dinosaur${selectedDinosaur + 1}`]} ${activeDinosaur === selectedDinosaur ? styles.activeBone : ""
-                    }`}
-                >
-                  <div
-                    className={`${styles[`dinosaur${selectedDinosaur + 1}Bone`]} ${activeDinosaur === selectedDinosaur ? styles.activeBone : ""
-                      }`}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        {isModalOpen &&
+            <XRayModal
+              isOpen={isModalOpen}
+              onClose={closeModal}
+              selectedDinosaur={selectedDinosaur}
+              activeDinosaur={activeDinosaur}
+              setActiveDinosaur={setActiveDinosaur}
+              
+            />
+        }
       </div>
     </div>
   );
