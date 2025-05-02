bc616bb80c311a62286b403d3627f2bd1b028bd2
Gemdelle <milu200911@hotmail.com>
2025-05-02 03:04:39 -0300

    game Img.

--- src/pages/public/Games/Games.tsx
+++ src/pages/public/Games/Games.tsx
@@ -1,14 +1,29 @@
 import { Nav } from "../../../components";
 import styles from "./Games.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Games = () => {
     return (
         <div className={styles.gamesPage}>
             <Nav />
             <div className={styles.gamesContainer}>
-                <Link to="/puzzleaurus" className={styles.puzzleaurus} />
-                <Link to="/memodyn" className={styles.memodyn} />
+                <div className={styles.gameContainer}>
+                    <div className={styles.gameImgContainer}>
+                        <Link to="/puzzleaurus" className={styles.gameImg} />
+                    </div>
+                    <img src="/public/assets/img/gamesPage/gameNameDivisionLine.png" alt="gold line" className={styles.goldLine} />
+                    <div className={styles.gameNameContainer}>
+                        <div className={styles.symbolContainer}>
+                            <div className={styles.symbolBg}></div>
+                            <img src="/public/assets/img/puzzles/puzzle-piece.png" alt="puzzle piece" className={styles.symbol} />
+                        </div>
+                        <span className={styles.gameName}>PUZZLEAURUS</span>
+                    </div>
+
+                </div>
+                <div className={styles.gameContainer}>
+                    <Link to="/memodyn" className={styles.memodyn} />
+                </div>
             </div>
         </div>
     );
