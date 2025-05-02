8bd6c1050cccabc971806d6932521f88cc72862a
Gemdelle <milu200911@hotmail.com>
2025-05-02 04:20:50 -0300

    gamePage + Game components and css.

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import styles from "./Game.module.css";
+import { Link } from 'react-router-dom';
 
-export const Game = () => {
-  return (
-    <div>Game</div>
-  )
+type SimboloProps = {
+    src: string;
+    alt: string;
+    altura: string;
+}
+
+type ImagenProps = {
+    src: string;
+    alt: string;
+}
+
+type GameProps = {
+    nombre: string;
+    cuadro: string;
+    imagen: ImagenProps;
+    simbolo: SimboloProps;
+    link: string;
+}
+
+export const Game = ({ nombre, cuadro, imagen, simbolo, link }: GameProps) => {
+    return (
+        <div className={styles.gameContainer}>
+            <div className={styles.gameImgContainer} style={{ backgroundImage: `url(${cuadro})`}}>
+                <Link
+                    to={link}
+                    className={styles.gameImg}
+                    style={{ backgroundImage: `url(${imagen.src})` }}
+                />
+            </div>
+            <img src="/public/assets/img/gamesPage/gameNameDivisionLine.png" alt="gold line" className={styles.goldLine} />
+            <div className={styles.gameNameContainer}>
+                <div className={styles.symbolContainer}>
+                    <div className={styles.symbolBg}></div>
+                    <img src={simbolo.src} alt={simbolo.alt} className={styles.symbol} style={{ height: `${simbolo.altura}`}}/>
+                </div>
+                <span className={styles.gameName}>{nombre}</span>
+            </div>
+        </div>
+    )
 }
--- src/pages/public/Games/Games.tsx
+++ src/pages/public/Games/Games.tsx
@@ -1,29 +1,43 @@
 import { Nav } from "../../../components";
+import { Game } from "../../../components/Game/Game";
 import styles from "./Games.module.css";
 import { Link } from "react-router-dom";
 
 export const Games = () => {
+
+    const juegos = [{
+        nombre: "Puzzleaurus",
+        cuadro: "/public/assets/img/gamesPage/gameAcessFrame1.png",
+        imagen: {
+            src: "/public/assets/img/gamesPage/puzzleaurusAccessBg.png",
+            alt: "Puzzleaurus"
+        },
+        simbolo: {
+            src: "/public/assets/img/puzzles/puzzle-piece.png",
+            alt: "pieza de puzzle",
+            altura: "60%"
+        },
+        link: "/puzzleaurus"
+    }, {
+        nombre: "Memodyn",
+        cuadro: "/public/assets/img/gamesPage/gameAcessFrame2.png",
+        imagen: {
+            src: "/public/assets/img/gamesPage/memodynAccessBg.png",
+            alt: "Memodyn"
+        },
+        simbolo: {
+            src: "/public/assets/img/cardBase/cardBase.png",
+            alt: "cartas",
+            altura: "45%"
+        },
+        link: "/memodyn"
+    }]
+
     return (
         <div className={styles.gamesPage}>
             <Nav />
             <div className={styles.gamesContainer}>
-                <div className={styles.gameContainer}>
-                    <div className={styles.gameImgContainer}>
-                        <Link to="/puzzleaurus" className={styles.gameImg} />
-                    </div>
-                    <img src="/public/assets/img/gamesPage/gameNameDivisionLine.png" alt="gold line" className={styles.goldLine} />
-                    <div className={styles.gameNameContainer}>
-                        <div className={styles.symbolContainer}>
-                            <div className={styles.symbolBg}></div>
-                            <img src="/public/assets/img/puzzles/puzzle-piece.png" alt="puzzle piece" className={styles.symbol} />
-                        </div>
-                        <span className={styles.gameName}>PUZZLEAURUS</span>
-                    </div>
-
-                </div>
-                <div className={styles.gameContainer}>
-                    <Link to="/memodyn" className={styles.memodyn} />
-                </div>
+                {juegos.map((juego)=><Game nombre={juego.nombre} cuadro={juego.cuadro} imagen={juego.imagen} simbolo={juego.simbolo} link={juego.link}></Game>)}
             </div>
         </div>
     );
