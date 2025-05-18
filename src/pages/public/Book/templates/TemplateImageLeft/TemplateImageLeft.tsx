80ac53dc07bced0b3611ba53c2782db248abd581
Gemdelle <milu200911@hotmail.com>
2025-05-18 20:12:19 -0300

    adults book img + characteristics title update.

--- src/pages/public/Book/templates/TemplateImageLeft/TemplateImageLeft.tsx
+++ src/pages/public/Book/templates/TemplateImageLeft/TemplateImageLeft.tsx
@@ -13,7 +13,7 @@ export const TemplateImageLeft = ({ imageLeftSrc, title, text }: TemplateImageLe
     return (
         <div className={bookStyles.page}>
             <p className={styles.title}>{title}</p>
-            <p className={styles.subtitle}>HATCHING</p>
+            <p className={styles.subtitle}>CHARACTERISTICS</p>
             <img src={imageLeftSrc} alt="" className={styles["image-left"]} />
             <div className={styles.text}>{text}</div>
         </div>
--- src/pages/public/Library/Library.tsx
+++ src/pages/public/Library/Library.tsx
@@ -20,26 +20,26 @@ const books = [
             {
                 type: BookType.templateImageLeft,
                 title: "Ankylosaurus",
-                imageLeftSrc: "/public/assets/img/books/adults-3/ankylosaurus/ankylosaurus-birth.png",
+                imageLeftSrc: "/public/assets/img/books/adults-2/ankylosaurus/ankylosaurus-birth.png",
                 text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
             },
             {
                 type: BookType.templateImageBottomLeftAndTopRight,
                 upperText: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
                 foodName: "Food Name",
-                imageBottomLeftSrc: "/public/assets/img/books/adults-3/ankylosaurus/ankylosaurus-food-1.png",
-                imageTopRightSrc: "/public/assets/img/books/adults-3/ankylosaurus/ankylosaurus-food-2.png",
+                imageBottomLeftSrc: "/public/assets/img/books/adults-2/ankylosaurus/ankylosaurus-food-1.png",
+                imageTopRightSrc: "/public/assets/img/books/adults-2/ankylosaurus/ankylosaurus-food-2.png",
                 lowerText: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis."
             },
             {
                 type: BookType.templateImageLeft,
-                imageLeftSrc: "/public/assets/img/books/adults-3/ankylosaurus/ankylosaurus-characteristics.png",
+                imageLeftSrc: "/public/assets/img/books/adults-2/ankylosaurus/ankylosaurus-characteristics.png",
                 text: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\n\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
             },
             {
                 type: BookType.templateImageBottomRight,
                 upperText: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-                imageBottomRightSrc: "/public/assets/img/books/adults-3/ankylosaurus/ankylosaurus-behaviour.png",
+                imageBottomRightSrc: "/public/assets/img/books/adults-2/ankylosaurus/ankylosaurus-behaviour.png",
                 lowerText: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis."
             }
             // DINOSAUR 2 - Brachiosaurus
