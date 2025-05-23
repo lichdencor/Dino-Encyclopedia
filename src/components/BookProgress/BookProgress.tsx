039639fe7e88f3849175f5e39d5f32532bc5d36f
Hunny <gonzalo.contogrobly@gmail.com>
2025-05-23 18:46:26 -0300

    Arregla el indice de los books.

--- src/components/BookProgress/BookProgress.tsx
+++ src/components/BookProgress/BookProgress.tsx
@@ -1,4 +1,5 @@
 import styles from "./BookProgress.module.css";
+import React, { useRef, useEffect, useState } from "react";
 
 type BookProgressProps = {
   pages: number;
@@ -6,29 +7,48 @@ type BookProgressProps = {
 };
 
 export const BookProgress: React.FC<BookProgressProps> = ({ pages, progress }) => {
-  const petLeftPercentage = pages > 1 ? (progress / (pages - 1)) * 100 : 0;
+  const totalPaws = pages + 1;
+  const pawRefs = useRef<(HTMLImageElement | null)[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [petLeft, setPetLeft] = useState<string>("0px");
 
-  console.log(progress, 'progress')
+  useEffect(() => {
+    //Cuando se dibuja la pantalla toma los valores de paw y del dino
+    const paw = pawRefs.current[progress];
+    const container = containerRef.current;
+    if (paw && container) {
+      const pawRect = paw.getBoundingClientRect();
+      const containerRect = container.getBoundingClientRect();
+
+      const pawCenter = pawRect.left + pawRect.width / 2;
+      const left = pawCenter - containerRect.left;
+      setPetLeft(`${left}px`);
+    }
+  }, [progress, pages]);
 
   return (
     <div className={styles["container-book-progress"]}>
-
-      <div className={styles["container-line-progress"]}>
+      <div className={styles["container-line-progress"]} ref={containerRef}>
         <div
           className={styles["container-pet"]}
-          style={{ left: `${petLeftPercentage}%`, transform: "translateX(-50%)" }}
+          style={{ 
+            left: petLeft,
+            transform: "translateX(-50%)"
+          }}
         >
           <img
             src="/public/assets/img/evolutions/profile/profile-t-rex-1-flipped.png"
-            alt="pet" className={styles["img-pet"]}
+            alt="pet" 
+            className={styles["img-pet"]}
           />
         </div>
 
         <div className={styles["line-progress"]}></div>
 
-        {[...Array(pages + 1)].map((_, index) => (
+        {[...Array(totalPaws)].map((_, index) => (
           <img
             key={index}
+            ref={el => pawRefs.current[index] = el}
             src={
               index < progress
                 ? "/public/assets/img/paws/level-paw-complete.png"
