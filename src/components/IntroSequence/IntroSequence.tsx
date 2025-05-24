fc3666bba2c53038a9888e04b78a7c2debd00b7a
Hunny <gonzalo.contogrobly@gmail.com>
2025-05-24 12:31:25 -0300

    Agrega secuencia de intro de imagenes.

---
+++ src/components/IntroSequence/IntroSequence.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import styles from './IntroSequence.module.css';
+
+const introImages = [
+  '/public/assets/img/intro/intro_01.jpg',
+  '/public/assets/img/intro/intro_02.jpg',
+  '/public/assets/img/intro/intro_03.jpg',
+  '/public/assets/img/intro/intro_04.jpg',
+  '/public/assets/img/intro/intro_05.jpg',
+  '/public/assets/img/intro/intro_06.jpg',
+];
+
+export const IntroSequence: React.FC = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const transitionTime = 6000; // 6 segundos por imagen
+    const timer = setInterval(() => {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % introImages.length);
+        setIsTransitioning(false);
+      }, 1000); // 1 segundo para el fade out/in
+    }, transitionTime);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className={styles.introSequence}>
+      <div 
+        className={`${styles.imageContainer} ${isTransitioning ? styles.transitioning : ''}`}
+        style={{
+          backgroundImage: `url(${introImages[currentImageIndex]})`,
+        }}
+      />
+    </div>
+  );
+};
+
+export default IntroSequence; 
